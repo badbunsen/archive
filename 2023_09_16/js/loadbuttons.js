83e0453a4aae919676bf9ce71a097a162e84e052
Aleph Diallo <aleph@rubberocket.com>
2025-02-03 08:09:27 -0500

    tweaking some stuff about this version

--- 2023_09_16/js/loadbuttons.js
+++ 2023_09_16/js/loadbuttons.js
@@ -1,10 +1,10 @@
 //Import buttons
-var button1 = "<a href='/'><img src='/images/buttons/home.svg' alt='homepage' width='auto' height='100%'></a>";
-var button2 = "<a href='/cartoons'><img src='/images/buttons/cartoons.svg' alt='cartoons' width='auto' height='100%'></a>";
-var button3 = "<a href='/artbook'><img src='/images/buttons/artbook.svg' alt='artbook' width='auto' height='100%'></a>";
-var button4 = "<a href='/games'><img src='/images/buttons/games.svg' alt='games' width='auto' height='100%'></a>";
-var button5 = "<a href='/work'><img src='/images/buttons/work.svg' alt='work' width='auto' height='100%'></a>";
-var button6 = "<a href='/goodies'><img src='/images/buttons/downloads.svg' width='auto' alt='downloads' height='100%'></a>";
-var button7 = "<a href='/about'><img src='/images/buttons/about.svg' alt='about' width='auto' height='100%'></a>";
+var button1 = "<a href='/archive/2023_09_16/'><img src='/archive/2023_09_16/images/buttons/home.svg' alt='homepage' width='auto' height='100%'></a>";
+var button2 = "<a href='/archive/2023_09_16/cartoons'><img src='/archive/2023_09_16/images/buttons/cartoons.svg' alt='cartoons' width='auto' height='100%'></a>";
+var button3 = "<a href='/archive/2023_09_16/artbook'><img src='/archive/2023_09_16/images/buttons/artbook.svg' alt='artbook' width='auto' height='100%'></a>";
+var button4 = "<a href='/archive/2023_09_16/games'><img src='/archive/2023_09_16/images/buttons/games.svg' alt='games' width='auto' height='100%'></a>";
+var button5 = "<a href='/archive/2023_09_16/work'><img src='/archive/2023_09_16/images/buttons/work.svg' alt='work' width='auto' height='100%'></a>";
+var button6 = "<a href='/archive/2023_09_16/goodies'><img src='/archive/2023_09_16/images/buttons/downloads.svg' width='auto' alt='downloads' height='100%'></a>";
+var button7 = "<a href='/archive/2023_09_16/about'><img src='/archive/2023_09_16/images/buttons/about.svg' alt='about' width='auto' height='100%'></a>";
 var buttonspan = document.getElementById("buttons");
 buttonspan.innerHTML = button1 + button2 + button3 + button4 + button5 + button6 + button7;
